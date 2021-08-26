f5f9b2307a82d94d21ff0f5640cb5d8f9f1c8e36
BayesianDetection <78782359+BayesianDetection@users.noreply.github.com>
2021-08-26 17:27:22 -0400

    Add files via upload

--- attacker/pgd.py
+++ attacker/pgd.py
@@ -3,6 +3,32 @@ import torch.nn.functional as F
 from .linf_sgd import Linf_SGD
 from torch.optim import Adam
 
+
+def Linf_Detect(x_in, y_true, z_true, net, steps, eps, L):
+    if eps == 0:
+        return x_in
+    training = net.training
+    if training:
+        net.eval()
+    x_adv = x_in.clone().requires_grad_()
+    optimizer = Linf_SGD([x_adv], lr=0.007)
+    for _ in range(steps):
+        optimizer.zero_grad()
+        net.zero_grad()
+        decision, out = net(x_adv)
+        loss = -F.cross_entropy(out, y_true)-L*F.cross_entropy(decision, z_true)
+        loss.backward()
+        optimizer.step()
+        diff = x_adv - x_in
+        diff.clamp_(-eps, eps)
+        x_adv.detach().copy_((diff + x_in).clamp_(0, 1))
+    net.zero_grad()
+    # reset to the original state
+    if training:
+        net.train()
+    return x_adv
+
+
 # performs Linf-constraint PGD attack w/o noise
 # @epsilon: radius of Linf-norm ball
 def Linf_PGD(x_in, y_true, net, steps, eps):
